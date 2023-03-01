1edd39bd2ff3399d7747c78cbdccfb2321389bd7
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-01 22:54:58 +0800

    Create 1.27.scm

---
+++ 1.27.scm
@@ -0,0 +1,75 @@
+；The following program searches for the top 10 carmichael numbers.It passes all Fermat's tests, but it's not prime.
+#lang racket
+
+(define (square x) (* x x))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))  
+
+(define (carmichael-test n)
+  (define (fermat-test n a)
+    (= (expmod a n n) a))
+  
+  (define (iter-test n a)
+    (cond ((= a 0) #t)
+          ((fermat-test n a) (iter-test n (- a 1)))
+          (else #f)))
+  
+  (iter-test n (- n 1)))
+
+(define (search-for-carmichae n count)
+  (cond ((not (= count 0))
+         (cond ((and (not (prime? n)) (carmichael-test n))
+                (displayln n)
+                (search-for-carmichae (+ n 1) (- count 1)))
+               (else (search-for-carmichae (+ n 1) count))))))
+
+;;;;;;;;;;;;;;;;;
+(search-for-carmichae 1 10)
+
+；The output is as follows.
+561
+1105
+1729
+2465
+2821
+6601
+8911
+10585
+15841
+29341
+
+；It's factorized into
+561 = 3 × 11 × 17
+1105 = 5 × 13 × 17
+1729 = 7 × 13 × 19
+2465 = 5 × 17 × 29
+2821 = 7 × 13 × 31
+6601 = 7 × 23 × 41
+8911 = 7 × 19 × 67
+10585 = 5 × 29 × 73
+15841 = 7 × 31 × 73
+29341 = 13 × 37 × 61
+
+；In the above program, if the search-for-carmichae judgment is reversed and written as (and (carmichael-test n) (not (prime?n)))) will be much slower.Is this because of the judgment prime?It's faster than carmichael-test.When it is prime, carmichael-test must be true, which involves extra operations.
+；DrRacket's judgment is short-circuit evaluation, when written as (and (not (prime?n)) (carmichael-test n)).If it is prime, carmichael-test is skipped.
