571acf6a2927c7a4852b84e156e6ece34abea062
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-03 23:20:30 +0800

    Create 2.4.scm

---
+++ chapter 2/2.4.scm
@@ -0,0 +1,21 @@
+；Another procedural representation of cons, car, and cdr is as follows
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+;The calculation procedure of expansion (car (cons x y)) is
+(car (cons x y))
+(car (lambda (m) (m x y)))
+((lambda (m) (m x y)) (lambda (p q) p))
+((lambda (p q) p) x y)
+x
+;The calculation process of expansion (cdr (cons x y)) is
+(cdr (cons x y))
+(cdr (lambda (m) (m x y)))
+((lambda (m) (m x y)) (lambda (p q) q))
+((lambda (p q) q) x y)
+y
