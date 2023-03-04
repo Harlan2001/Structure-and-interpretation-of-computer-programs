4fb23ae0c2e8326888ce44f1089b58d05c28d7e8
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-04 14:28:58 +0800

    Create 2.21.scm

---
+++ chapter 2/2.21.scm
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (square x)
+  (* x x))
+
+(define (square-list items)
+  (if (null? items)
+      null
+      (cons (square (car items))
+            (square-list (cdr items)))))
+
+
+(define (square-list-2 items)
+  (map square items))
+
+;;;;;;;;;;;;;;;;;
+(square-list (list 1 2 3 4 5))
+(square-list-2 (list 1 2 3 4 5))
