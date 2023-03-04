f0732f6037cd5ed20df9787a79d40b6baf30efaf
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-04 14:28:36 +0800

    Create 2.20.scm

---
+++ chapter 2/2.20.scm
@@ -0,0 +1,19 @@
+
+#lang racket
+
+
+(define (filter proc items)
+  (if (null? items)
+      null
+      (if (proc (car items))
+          (cons (car items) (filter proc (cdr items)))
+          (filter proc (cdr items)))))
+
+(define (same-parity a . w)
+  (cons a 
+        (filter (lambda (x) (even? (+ a x)))
+                w)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(same-parity 1 2 3 4 5 7 9 10)
+(same-parity 2 3 4 5 6 7 8 9 10)
