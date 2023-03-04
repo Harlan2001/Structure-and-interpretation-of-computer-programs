aa0d83636c40a1db6fc3ecd8a4ee0697920c5489
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-04 14:35:25 +0800

    Create 2.28.scm

---
+++ chapter 2/2.28.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (fringe items)
+  (cond ((null? items) null)
+        ((not (pair? items)) (list items))
+        (else (append (fringe (car items))
+                      (fringe (cdr items))))))
+
+
+;;;;;;;;;;;;;;
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+
+(fringe (list x x))
