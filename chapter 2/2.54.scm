9e0ef9ac528f5a120b879bf7bad8f2a6805a84d5
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-05 12:07:25 +0800

    Create 2.54.scm

---
+++ chapter 2/2.54.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (equal? a b)
+  (cond ((and (null? a) (null? b)) #t)
+        ((and (symbol? a) (symbol? b)) (eq? a b))
+        ((and (pair? a) (pair? b))
+          (and (equal? (car a) (car b))
+               (equal? (cdr a) (cdr b))))
+        (else #f)))
+
+;;;;;;;;;;;;;;;;;;;;
+(equal? '(this is a list) '(this is a list))
+(equal? '(this is a list) '(this (is a) list))
+(equal? '(this is a list) '(this))
