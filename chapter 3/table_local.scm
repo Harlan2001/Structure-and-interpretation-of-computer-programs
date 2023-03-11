e24285f79257a4aa1f59958e5aca690327a405fc
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-11 21:33:43 +0800

    Create table_local.scm

---
+++ chapter 3/table_local.scm
@@ -0,0 +1,51 @@
+#lang sicp
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable 
+                            (cons (cons key-2 value) (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table))))))
+    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;The numbers are ASCII, the + symbol is 43, the a symbol is 97, etc
+(put 'math '+ 43)
+(put 'math '- 45)
+(put 'math '* 42)
+
+(put 'letters 'a 97)
+(put 'letters 'b 98)
+
+(get 'math '-)
+(get 'letters 'b)
