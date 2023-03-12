fda398c15532ad45619678067a274df271441519
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-12 13:57:28 +0800

    Create 3.59.scm

---
+++ chapter 3/3.59.scm
@@ -0,0 +1,36 @@
+#lang racket
+
+(require "stream.scm")
+(require "infinite_stream.scm")
+(provide exp-series cosine-series sine-series)
+
+(define (div-stream s1 s2)
+  (stream-map / s1 s2))
+
+(define (neg-stream s)
+  (scale-stream s -1))
+
+; a)
+(define (integrate-series s)
+  (div-stream s integers))
+
+; b)
+(define exp-series
+  (cons-stream 1 (integrate-series exp-series)))
+
+(define cosine-series
+  (cons-stream 1 (neg-stream (integrate-series sine-series))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
+
+;;;;;;;;;;;;;;;;;;;;;
+(module* main #f
+  (define ones (cons-stream 1 ones))
+  (stream-head->list (neg-stream ones) 10)       
+  (stream-head->list (integrate-series ones) 10) 
+
+  (stream-head->list exp-series 10)
+  (stream-head->list cosine-series 10) 
+  (stream-head->list sine-series 10)
+)
