84fe14f375b35cf7b5a3afe3ba4047217de88590
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-16 09:48:00 +0800

    Create 4.62.scm

---
+++ chapter 4/4.62.scm
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require "queryeval.scm")
+
+(initialize-data-base 
+  '(
+    (rule (last-pair (?x) (?x)))
+    
+    (rule (last-pair (?u . ?v) ?x)
+          (last-pair ?v ?x))
+    ))
+
+(easy-qeval '(last-pair (3) ?x))
+; (last-pair (3) (3))
+
+(easy-qeval '(last-pair (1 2 3) ?x))
+; (last-pair (1 2 3) (3))
+
+(easy-qeval '(last-pair (2 ?x) (3)))
+; (last-pair (2 3) (3))
+
+; (easy-qeval '(last-pair ?x (3)))
+; Will not stop
