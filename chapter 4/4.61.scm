911583079256937bcd8beb4248ec6bb4e3750ffd
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-16 09:47:36 +0800

    Create 4.61.scm

---
+++ chapter 4/4.61.scm
@@ -0,0 +1,21 @@
+#lang sicp
+
+(#%require "queryeval.scm")
+
+(initialize-data-base 
+  '(
+    (rule (?x next-to ?y in (?x ?y . ?u)))
+    
+    (rule (?x next-to ?y in (?v . ?z))
+          (?x next-to ?y in ?z))
+    ))
+
+(easy-qeval '(?x next-to ?y in (1 (2 3) 4)))
+; Query output: 
+; ((2 3) next-to 4 in (1 (2 3) 4)) 
+; (1 next-to (2 3) in (1 (2 3) 4)) 
+
+(easy-qeval '(?x next-to 1 in (2 1 3 1)))
+; Query results:
+; (3 next-to 1 in (2 1 3 1))
+; (2 next-to 1 in (2 1 3 1))
