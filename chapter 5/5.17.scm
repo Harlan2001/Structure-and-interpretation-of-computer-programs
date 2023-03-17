d3f51deca423166b2522bad964ba92c90ecdf509
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-17 10:10:02 +0800

    Create 5.17.scm

---
+++ chapter 5/5.17.scm
@@ -0,0 +1,171 @@
+#lang sicp
+
+(#%require "ch5support.scm")
+(#%require "ch5-regsim.scm")
+
+;;In make-instruction, add labels.And label is set during the extract-labels process.
+
+(define (make-machine register-names ops controller-text)
+  (let ((machine (make-new-machine)))
+    (for-each (lambda (register-name)
+                ((machine 'allocate-register) register-name))
+              register-names)
+    ((machine 'install-operations) ops)    
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (instruction-number 0)
+        (trace-on false))
+    (let ((the-ops
+            (list (list 'initialize-stack
+                        (lambda () (stack 'initialize)))
+                  (list 'print-stack-statistics
+                        (lambda () (stack 'print-statistics)))
+                  (list 'print-instruction-number
+                        (lambda () 
+                          (display "instruction-number: ")
+                          (display instruction-number)
+                          (newline)
+                          (set! instruction-number 0)))))
+          (register-table
+            (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                (cond (trace-on
+                        (if (null? (instruction-label (car insts)))
+                            (begin
+                              (display (instruction-text (car insts)))
+                              (newline))
+                            (begin
+                              (display (instruction-label (car insts)))
+                              (newline)
+                              (display (instruction-text (car insts)))
+                              (newline)))))
+                (set! instruction-number (+ instruction-number 1))
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              ((eq? message 'trace-on) (set! trace-on true))
+              ((eq? message 'trace-off) (set! trace-on false))
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+(define (assemble controller-text machine)
+  (extract-labels controller-text
+                  (lambda (insts labels)
+                    (update-insts! insts labels machine)
+                    insts)))
+
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels 
+        (cdr text)
+        (lambda (insts labels)
+          (let ((next-inst (car text)))
+            (if (symbol? next-inst)
+                (begin
+                  (set-instruction-label! (car insts) next-inst)
+                  (receive insts
+                           (cons (make-label-entry next-inst insts) labels)))
+                (receive (cons (make-instruction next-inst) insts)
+                         labels)))))))
+
+(define (update-insts! insts labels machine)
+  (let ((pc (get-register machine 'pc))
+        (flag (get-register machine 'flag))
+        (stack (machine 'stack))
+        (ops (machine 'operations)))
+    (for-each
+      (lambda (inst)
+        (set-instruction-execution-proc! 
+          inst
+          (make-execution-procedure
+            (instruction-text inst) labels machine
+            pc flag stack ops)))
+      insts)))
+
+(define (make-instruction text)
+  (list text '() '()))
+
+(define (instruction-text inst)
+  (car inst))
+
+(define (instruction-execution-proc inst)
+  (cadr inst))
+
+(define (set-instruction-execution-proc! inst proc)
+  (set-car! (cdr inst) proc))
+
+(define (instruction-label inst)
+  (caddr inst))
+
+(define (set-instruction-label! inst label)
+  (set-car! (cddr inst) label))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define fact-machine
+  (make-machine
+    '(continue n val)
+    (list (list '= =) (list '- -) (list '* *))
+    '(
+      (assign continue (label fact-done))     ; set up final return address
+      fact-loop
+      (test (op =) (reg n) (const 1))
+      (branch (label base-case))
+      ;; Set up for the recursive call by saving n and continue.
+      ;; Set up continue so that the computation will continue
+      ;; at after-fact when the subroutine returns.
+      (save continue)
+      (save n)
+      (assign n (op -) (reg n) (const 1))
+      (assign continue (label after-fact))
+      (goto (label fact-loop))
+      after-fact
+      (restore n)
+      (restore continue)
+      (assign val (op *) (reg n) (reg val))   ; val now contains n(n - 1)!
+      (goto (reg continue))                   ; return to caller
+      base-case
+      (assign val (const 1))                  ; base case: 1! = 1
+      (goto (reg continue))                   ; return to caller
+      fact-done
+      (perform (op print-instruction-number))
+      )))
+
+(set-register-contents! fact-machine 'n 10)
+(fact-machine 'trace-on)
+(start fact-machine)
+(get-register-contents fact-machine 'val)
