0870040ef92834ff85beb6ea96361dab97cbd9e8
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-03-19 11:52:00 +0800

    Create prime_libs.h

---
+++ chapter 5/exercise_5_51/prime_libs.h
@@ -0,0 +1,29 @@
+// Native function implementation
+
+#ifndef __SCHEME_LIBS_H__
+#define __SCHEME_LIBS_H__
+
+#include "scheme_runtime.h"
+
+struct PrimitiveProc {
+    const char *symbol;
+    Primitive proc;
+};
+struct PrimitiveProc *get_primitive_procedures(int *size);
+void display_primitive(Primitive proc);
+
+int length(Reg *list);
+void reverse(Reg *result, Reg *list);
+void append(Reg *result, Reg *list0, Reg *list1);
+
+void for_each(Reg *args, void *userdata, void (*callback)(void *userdata, Reg *item));
+
+Reg *cadr(Reg *reg, Reg *pair);
+Reg *cddr(Reg *reg, Reg *pair);
+Reg *caddr(Reg *reg, Reg *pair);
+Reg *caadr(Reg *reg, Reg *pair);
+Reg *cdadr(Reg *reg, Reg *pair);
+Reg *cdddr(Reg *reg, Reg *pair);
+Reg *cadddr(Reg *reg, Reg *pair);
+
+#endif
